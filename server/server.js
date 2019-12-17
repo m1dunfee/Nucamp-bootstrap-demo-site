5a16124f8b174a1690badcae25bfc8de1edbc288
Matthew Dunfee <Matthewgoldshire@hotmail.com>
2019-12-17 14:47:05 -0600

    build out server and added CDNs instead of node packages

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+
+// Serve static files
+app.use(express.static('public'));
+
+// App Set //
+const PORT = process.env.PORT || 5000;
+
+/** Listen * */
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
